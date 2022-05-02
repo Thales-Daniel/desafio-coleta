31ed079fd1018c04e3ff5a2e029ca2ba9b8ae9e9
thales <thalesdaniel2013@gmail.com>
2022-05-01 21:40:05 -0300

    feat: adicionado logica para mudança de pagina

--- src/pages/form/index.jsx
+++ src/pages/form/index.jsx
@@ -1,4 +1,5 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Buttons from '../../components/buttons';
 import Select from '../../components/select';
 import TextArea from '../../components/textArea';
@@ -6,14 +7,26 @@ import { StateContext } from '../../context/StateProvider';
 import './style.css';
 
 function Form() {
+  const [redireciona, setRediciona] = useState(false);
   const { verificaCampos } = useContext(StateContext);
+  const navigate = useNavigate();
+
+  const Submit = (e) => {
+    e.preventDefault();
+    setRediciona(true);
+  };
+
   useEffect(() => {
-    console.log(verificaCampos);
-  }, [verificaCampos]);
+    if (!redireciona) {
+      return undefined;
+    }
+    return navigate('/customer');
+  }, [verificaCampos, redireciona]);
+
   const verificaTodosCampos = Object.values(verificaCampos).some((item) => item === false);
 
   return (
-    <form className="form">
+    <form className="form" onSubmit={Submit}>
       <h1>Desafio Coleta - GRX</h1>
       <Buttons />
       <Select />
